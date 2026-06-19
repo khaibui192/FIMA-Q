b343957a343a0f0a5cd26c8bf6dff8f9f09ea633
khaibui192 <khaiquang.dev@gmail.com>
2026-06-19 10:13:26 +0700

    feat

--- configs/3bit/best.py
+++ configs/3bit/best.py
@@ -3,8 +3,10 @@ class Config:
         # calibration settings
         self.optim_size = 1024
         self.calib_size = 128
-        self.optim_batch_size = 32
-        self.calib_batch_size = 32
+        # self.optim_batch_size = 32
+        # self.calib_batch_size = 32
+        self.optim_batch_size = 16
+        self.calib_batch_size = 16
         self.w_bit = 3
         self.a_bit = 3
         self.qconv_a_bit = 8
@@ -12,8 +14,10 @@ class Config:
         self.calib_metric = 'mse'
         self.matmul_head_channel_wise = True
         self.token_channel_wise = True
-        self.eq_n = 128
-        self.search_round = 3
+        # self.eq_n = 128
+        # self.search_round = 3
+        self.eq_n = 64
+        self.search_round = 1
         # optimization settings
         self.keep_gpu = True
         self.optim_metric = 'fisher_dplr'
